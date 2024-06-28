42c73cc079d118b4dc9f6fe776769c5768ec0ae7
rafaellevissa <rafaellevissa@gmail.com>
2024-06-28 20:36:47 -0300

    refactor: rename controller, module and service

--- services/product/src/product/product.controller.spec.ts
+++ services/product/src/product/product.controller.spec.ts
@@ -1,20 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
 
-describe('AppController', () => {
+describe('ProductController', () => {
   let app: TestingModule;
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
+      controllers: [ProductController],
+      providers: [ProductService],
     }).compile();
   });
 
   describe('getHello', () => {
     it('should return "Hello World!"', () => {
-      const appController = app.get(AppController);
+      const appController = app.get(ProductController);
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
